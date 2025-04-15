bfea26140550365627174a28101d087ad318f869
Zychronix <zychronix41@gmail.com>
2025-04-15 13:56:23 -0400

    Merge main

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,18 +28,19 @@ repositories {
   mavenCentral()
   mavenLocal() // Uncomment to use mavenLocal version of LoD engine
   maven { url = uri("https://jitpack.io") }
+  maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 dependencies {
   implementation("legend:lod:snapshot") // Uncomment to use mavenLocal version of LoD engine (also comment out next line)
 //  implementation("com.github.Legend-of-Dragoon-Modding:Legend-of-Dragoon-Java:main-SNAPSHOT")
   implementation("com.opencsv:opencsv:5.7.1")
-  implementation("org.fusesource.jansi:jansi:2.3.2")
-  implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-  implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+  implementation("org.fusesource.jansi:jansi:2.4.1")
+  implementation("org.apache.logging.log4j:log4j-api:2.24.3")
+  implementation("org.apache.logging.log4j:log4j-core:2.24.3")
   implementation("com.google.code.findbugs:jsr305:3.0.2")
-  implementation("com.github.JnCrMx:discord-game-sdk4j:v0.5.5")
-  api("org.legendofdragoon:mod-loader:4.1.0")
+  implementation("com.github.JnCrMx:discord-game-sdk4j:v1.0.0")
+  api("org.legendofdragoon:mod-loader:4.2.0")
   api("org.legendofdragoon:script-recompiler:0.5.6")
 }
 
