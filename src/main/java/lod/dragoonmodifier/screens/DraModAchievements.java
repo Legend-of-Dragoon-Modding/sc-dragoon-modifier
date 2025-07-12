4e04b044047dd681d75870f56270a1279160bb42
Zychronix <zychronix41@gmail.com>
2025-07-12 11:05:37 -0400

    Init test version

---
+++ src/main/java/lod/dragoonmodifier/screens/DraModAchievements.java
@@ -0,0 +1,31 @@
+package lod.dragoonmodifier.screens;
+
+import legend.game.combat.ui.UiBox;
+import legend.game.inventory.screens.FontOptions;
+import legend.game.inventory.screens.HorizontalAlign;
+import legend.game.inventory.screens.MenuScreen;
+import legend.game.inventory.screens.TextColour;
+
+import static legend.game.Scus94491BpeSegment_8002.renderText;
+
+public class DraModAchievements extends MenuScreen {
+
+  final UiBox ui;
+  final int trophyType;
+  final String message;
+  final boolean show;
+  final FontOptions font = new FontOptions().colour(TextColour.WHITE).shadowColour(TextColour.BLACK).horizontalAlign(HorizontalAlign.LEFT).size(0.5f);
+
+  public DraModAchievements(final int trophyType, final String message, final boolean show) {
+    this.trophyType = trophyType;
+    this.message = message;
+    this.show = show;
+    this.ui = new UiBox("DraMod Achievement", 0, 232, 368, 8);
+  }
+
+  @Override
+  protected void render() {
+    renderText(this.message, 0, 234, new FontOptions().colour(TextColour.WHITE).shadowColour(TextColour.BLACK).horizontalAlign(HorizontalAlign.LEFT).size(0.5f));
+    this.ui.render(255, 255, 0);
+  }
+}
