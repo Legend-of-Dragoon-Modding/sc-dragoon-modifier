15a3f32f9e7ffd492860aeef0feb82862c2bdf0f
Zychronix <zychronix41@gmail.com>
2025-07-30 17:59:46 -0400

    Update DraModSaveFile.java

--- src/main/java/lod/dragoonmodifier/saves/DraModSaveFile.java
+++ src/main/java/lod/dragoonmodifier/saves/DraModSaveFile.java
@@ -1,12 +1,14 @@
 package lod.dragoonmodifier.saves;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DraModSaveFile implements Serializable {
   public int monsterArenaStage = 0;
   public int huntQuestStage = 0;
   public int ultimateBossStage = 0;
   public int faustDefeated = 0;
-  public boolean[] baseGameAchievements = new boolean[255];
-  public boolean[] enhancedAchievements = new boolean[255];
+  public Map<Integer, Boolean> baseGameAchievements = new HashMap<>();
+  public Map<Integer, Boolean> enhancedAchievements = new HashMap<>();
 }
